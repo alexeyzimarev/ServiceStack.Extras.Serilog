7cdc3badf99bbe47780143841a823cba77946cb4
Alexey Zimarev <alzi@abax.no>
2016-10-24 15:31:02 +0200

    Fixed empty messages

--- src/ServiceStack.Extras.Serilog/SerilogLogger.cs
+++ src/ServiceStack.Extras.Serilog/SerilogLogger.cs
@@ -15,21 +15,89 @@ namespace ServiceStack.Extras.Serilog
             _log = log;
         }
 
-        public void Debug(object message) => _log.Debug(string.Empty, message);
-        public void Debug(object message, Exception exception) => _log.Debug(exception, string.Empty, message);
-        public void DebugFormat(string format, params object[] args) => _log.Debug(format, args);
-        public void Error(object message) => _log.Error(string.Empty, message);
-        public void Error(object message, Exception exception) => _log.Error(exception, string.Empty, message);
-        public void ErrorFormat(string format, params object[] args) => _log.Error(format, args);
-        public void Fatal(object message) => _log.Fatal(string.Empty, message);
-        public void Fatal(object message, Exception exception) => _log.Fatal(exception, string.Empty, message);
-        public void FatalFormat(string format, params object[] args) => _log.Fatal(format, args);
-        public void Info(object message) => _log.Information(string.Empty, message);
-        public void Info(object message, Exception exception) => _log.Information(exception, string.Empty, message);
-        public void InfoFormat(string format, params object[] args) => _log.Information(format, args);
-        public void Warn(object message) => _log.Warning(string.Empty, message);
-        public void Warn(object message, Exception exception) => _log.Warning(exception, string.Empty, message);
-        public void WarnFormat(string format, params object[] args) => _log.Warning(format, args);
+        public void Debug(object message)
+        {
+            if (IsDebugEnabled) _log.Debug(message.ToString());
+        }
+
+        public void Debug(object message, Exception exception)
+        {
+            if (IsDebugEnabled) _log.Debug(exception, message.ToString());
+        }
+
+        public void DebugFormat(string format, params object[] args)
+        {
+            if (IsDebugEnabled) _log.Debug(format, args);
+        }
+
+        public void Error(object message)
+        {
+            if (IsErrorEnabled) _log.Error(message.ToString());
+        }
+
+        public void Error(object message, Exception exception)
+        {
+            if (IsErrorEnabled) _log.Error(exception, message.ToString());
+        }
+
+        public void ErrorFormat(string format, params object[] args)
+        {
+            if (IsErrorEnabled) _log.Error(format, args);
+        }
+
+        public void Fatal(object message)
+        {
+            if (IsFatalEnabled) _log.Fatal(message.ToString());
+        }
+
+        public void Fatal(object message, Exception exception)
+        {
+            if (IsFatalEnabled) _log.Fatal(exception, message.ToString());
+        }
+
+        public void FatalFormat(string format, params object[] args)
+        {
+            if (IsFatalEnabled) _log.Fatal(format, args);
+        }
+
+        public void Info(object message)
+        {
+            if (IsInfoEnabled) _log.Information(message.ToString());
+        }
+
+        public void Info(object message, Exception exception)
+        {
+            if (IsInfoEnabled) _log.Information(exception, message.ToString());
+        }
+
+        public void InfoFormat(string format, params object[] args)
+        {
+            if (IsInfoEnabled) _log.Information(format, args);
+        }
+
+        public void Warn(object message)
+        {
+            if (IsWarnEnabled) _log.Warning(message.ToString());
+        }
+
+        public void Warn(object message, Exception exception)
+        {
+            if (IsWarnEnabled) _log.Warning(exception, message.ToString());
+        }
+
+        public void WarnFormat(string format, params object[] args)
+        {
+            if (IsWarnEnabled) _log.Warning(format, args);
+        }
+
         public bool IsDebugEnabled => _log.IsEnabled(LogEventLevel.Debug);
+
+        public bool IsInfoEnabled => _log.IsEnabled(LogEventLevel.Information);
+
+        public bool IsWarnEnabled => _log.IsEnabled(LogEventLevel.Warning);
+
+        public bool IsErrorEnabled => _log.IsEnabled(LogEventLevel.Error);
+
+        public bool IsFatalEnabled => _log.IsEnabled(LogEventLevel.Fatal);
     }
 }
